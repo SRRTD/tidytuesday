7e0ed33d24c26ae71fb7df08c2608c2c3cb6ae49
SRRTD <noregency@gmail.com>
2025-01-08 17:56:35 -0500

    rethink the whole thing goddamit

--- 25W1/SCRIPTS/plot.R
+++ 25W1/SCRIPTS/plot.R
@@ -30,24 +30,36 @@ chelsea_players %<>%
   unnest(cols = c(data)) %>%
   ungroup()
 
-chelsea_players %>%
-  select(md, player, goals, cumulative_goals) %>%
-  group_by(md) %>%
-  mutate(rank = rank(-cumulative_goals, ties.method = "first")) %>%
-  arrange(md) %>% 
-  ggplot(aes(x = rank, y = cumulative_goals)) +
-  geom_col(fill = "#034694", width = 0.5) +
-  geom_text(aes(y = cumulative_goals, label = player , hjust = -0.05), size = 5) +
-  geom_text(aes(y = cumulative_goals, label = round(cumulative_goals), hjust = 3), size = 5, color = "white") +
-  coord_flip() +
-  xlim(5.5, 0.5) +
-  ylim(0, 20) +
-  theme_minimal() +
-  theme(
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)) +
-  transition_states(md, state_length = 5, transition_length = 0, wrap = FALSE)
+#Turns out patchwork doesn't work with gganimate
 
+# goalscorers <-
+#   chelsea_players %>%
+#     select(md, player, goals, cumulative_goals) %>%
+#     group_by(md) %>%
+#     mutate(rank = rank(-cumulative_goals, ties.method = "first")) %>%
+#     arrange(md) %>%
+#     #filter(md == 15) %>%
+#     ggplot(aes(x = rank, y = cumulative_goals)) +
+#     geom_col(fill = "#034694", width = 0.5) +
+#     geom_text(aes(y = cumulative_goals, label = player , hjust = -0.05), size = 7) +
+#     geom_text(aes(y = cumulative_goals, label = cumulative_goals, hjust = 3), size = 8, color = "white") +
+#     coord_flip() +
+#     xlim(5.5, 0.5) +
+#     ylim(0, 20) +
+#     labs(title = "Main goalscorers",
+#          x = NULL,
+#          y = "Cumulative goals") +
+#     theme_minimal() +
+#     theme(
+#       axis.text.y = element_blank(),
+#       axis.ticks.y = element_blank(),
+#       plot.margin = margin(20, 20, 20, 20),
+#       panel.grid.major.y = element_blank(),
+#       panel.grid.minor.y = element_blank(),
+#       plot.title = element_text(size = 30, hjust = 0.5, vjust = 3),
+#       axis.text.x = element_text(size = 15),
+#       axis.title.x = element_text(size = 17, vjust = -2)) +
+#       transition_states(md, state_length = 10, transition_length = 0, wrap = FALSE)
+#   
+#   
 
-  
